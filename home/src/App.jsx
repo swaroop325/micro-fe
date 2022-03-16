1147106b139440c16944f19f4f646fb1ffd24a88
swaroop_a.s <swaroopanand325@gmail.com>
2022-03-16 12:51:38 +0530

    initialised MFE structure

---
+++ home/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
+import "./index.scss";
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header/>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <Footer/>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
